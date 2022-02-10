5fa5daf8294940bf3ea459964a1f6003deedf900
Thomas Selin <selin.reso@gmail.com>
2022-02-10 11:25:00 +0100

    Set columns to unique where necessary

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -3,26 +3,26 @@ create database golf_competition;
 use golf_competition;
 
 create table players(
-	social_security_number char(13) not null,
+	social_security_number char(13) not null unique,
     full_name varchar(20) not null,
     age tinyint not null,
     primary key(social_security_number)
 )engine=InnoDB;
 
 create table competitions(
-	competition_name varchar(20) not null,
+	competition_name varchar(20) not null unique,
     competition_date date not null,
     primary key(competition_name)
 )engine=InnoDB;
 
 create table weather_conditions(
-	weather_condition_type varchar(20) not null,
+	weather_condition_type varchar(20) not null unique,
     wind_speed tinyint,
     primary key(weather_condition_type)
 )engine=InnoDB;
 
 create table constructions(
-	serial_number smallint not null auto_increment,
+	serial_number smallint not null unique auto_increment,
     hardness varchar(10) not null,
     primary key (serial_number)
 )engine=InnoDB;
@@ -31,7 +31,7 @@ create table jackets(
 	brand varchar(20) not null,
     size varchar(10),
     material varchar(20),
-    player_social_security_number char(13) not null,
+    player_social_security_number char(13) not null unique,
     primary key(player_social_security_number, brand),
     foreign key(player_social_security_number) references players(social_security_number)
 )engine=InnoDB;
@@ -39,7 +39,7 @@ create table jackets(
 create table clubs(
 	club_number varchar(20) not null,
     material varchar(20) not null,
-    player_social_security_number char(13) not null,
+    player_social_security_number char(13) not null unique,
     construction_serial_number smallint not null,
     primary key(player_social_security_number, club_number),
     foreign key(player_social_security_number) references players(social_security_number),
